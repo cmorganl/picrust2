2abb4510e2c7f785c4962146f37f9928046e14be
cmorganl <c.morganlang@gmail.com>
2023-09-29 15:36:17 +0200

    Remove SEPP dependency
    Former-commit-id: 4aff3fa8ebdb9a28ee902236f614a9a0699ba19d

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ from setuptools import setup
 from glob import glob
 
 __license__ = "GPL"
-__version__ = "2.5.2"
+__version__ = "2.5.3"
 __maintainer__ = "Gavin Douglas"
+__doc__ = 'PICRUSt: Phylogenetic Investigation of Communities by Reconstruction of Unobserved States'
 
 long_description = ("Douglas et al. 2020. PICRUSt2 for prediction of "
                     "metagenome functions. Nature Biotechnology. doi: "
@@ -13,17 +14,20 @@ long_description = ("Douglas et al. 2020. PICRUSt2 for prediction of "
 
 setup(name='PICRUSt2',
       version=__version__,
-      description=('PICRUSt: Phylogenetic Investigation of Communities by '
-                   'Reconstruction of Unobserved States'),
+      description=__doc__,
       maintainer=__maintainer__,
       url='https://github.com/picrust/picrust2/wiki',
       packages=['picrust2'],
+      package_dir={'picrust2': 'picrust2'},
       scripts=glob('scripts/*py'),
       install_requires=['numpy',
-			'h5py',
+                        'h5py',
                         'joblib',
                         'biom-format'],
       package_data={'picrust2':
                     ['MinPath/MinPath12hmp.py',
-                     'Rscripts/*R']},
+                     'Rscripts/*R',
+                     'default_files/prokaryotic/*',
+                     'default_files/pathway_mapfiles/*',
+                     'default_files/description_mapfiles/*']},
       long_description=long_description)
